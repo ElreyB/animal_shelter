b7ff290cd4abd344ee18847c68d796cadc330bac
Elrey Belmonti <elbelmonti@gmail.com>
2017-09-21 18:14:34 -0700

    add .find to Animal that finds an aniaml by id

--- lib/animal.rb
+++ lib/animal.rb
@@ -26,6 +26,11 @@ class Animal
     Animal.map_animals(results)
   end
 
+  def self.find(id)
+    animal = DB.exec("SELECT * FROM animals WHERE id = #{id}")
+    Animal.map_animals(animal)
+  end
+
   def ==(other_animal)
     self.id == other_animal.id &&
     self.name == other_animal.name &&
